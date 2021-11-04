21b636162410f93247a65b568e859a9a9d14ac67
eloylopez <eloylopez@eloys-air.home>
2021-11-04 18:57:11 -0500

    Refactored Util files and added css sheet and main page html

---
+++ webproject-CyberSnake/src/util/UtilAccount.java
@@ -0,0 +1,86 @@
+package util;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+import datamodel.Account;
+
+public class UtilAccount extends UtilDB{
+	
+	//get all accounts
+	public static List<Account> listAccounts() {
+		List<Account> resultList = new ArrayList<Account>();
+
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null; 
+
+		try {
+			tx = session.beginTransaction();
+			List<?> accounts = session.createQuery("FROM Account").list();
+			for (Iterator<?> iterator = accounts.iterator(); iterator.hasNext();) {
+				Account account = (Account) iterator.next();
+				resultList.add(account);
+			}
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return resultList;
+	}
+
+	//get account with specified username
+	public static Account getAccount(String username) {
+		Account account = null;
+
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null;
+
+		try {
+			tx = session.beginTransaction();
+			
+			account = (Account) session.createCriteria(Account.class).add(Restrictions.eq("username", username)).uniqueResult();
+			
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return account;
+	}
+
+	//create a new account
+	public static boolean createAccount(String userName, String firstName, String lastName, String address, String city,
+			String state, String zip, String email, String phone, String password, String admin) {
+		Session session = getSessionFactory().openSession();
+		boolean result = true;
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			session.save(new Account(userName, firstName, lastName, address, city, state, Integer.valueOf(zip), email,
+					phone, password, Boolean.parseBoolean(admin)));
+			tx.commit();
+		} catch (HibernateException e) {
+			result = false;
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return result;
+	}
+
+}
---
+++ webproject-CyberSnake/src/util/UtilTimeslot.java
@@ -0,0 +1,89 @@
+package util;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+import java.sql.Date;
+import datamodel.Timeslot;
+
+
+
+public class UtilTimeslot extends UtilDB{
+	
+	//get all timeslots
+	public static List<Timeslot> listTimeslots() {
+		List<Timeslot> resultList = new ArrayList<Timeslot>();
+
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null;
+
+		try {
+			tx = session.beginTransaction();
+			List<?> timeslots = session.createQuery("FROM Timeslot").list();
+			for (Iterator<?> iterator = timeslots.iterator(); iterator.hasNext();) {
+				Timeslot timeslot = (Timeslot) iterator.next();
+				resultList.add(timeslot);
+			}
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return resultList;
+	}
+
+	//get one specific timeslot
+	public static Timeslot getTimeslot(int timeslotId) {
+		Timeslot timeslot = null;
+
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null;
+
+		try {
+			tx = session.beginTransaction();
+			
+			timeslot = (Timeslot) session.createCriteria(Timeslot.class).add(Restrictions.eq("timeslotId", timeslotId)).uniqueResult();
+			
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return timeslot;
+	}
+
+	//create timeslot
+	public static boolean createTimeslot(String eventId, String date, String time, String occupancy) {
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null;
+		boolean result = true;
+		
+		try {
+			tx = session.beginTransaction();
+			session.save(new Timeslot(Integer.valueOf(eventId), Date.valueOf(date), time, Integer.valueOf(occupancy)));
+			tx.commit();
+		} catch (HibernateException e) {
+			result = false;
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return result;
+	}
+
+
+}
