c4c15696945f991925e91082a4c7a54e93a930bf
eloylopez <eloylopez@eloys-air.home>
2021-11-07 17:48:21 -0600

    Login/logout functionality and added basic user account page

--- webproject-CyberSnake/src/util/UtilEvent.java
+++ webproject-CyberSnake/src/util/UtilEvent.java
@@ -12,9 +12,9 @@ import org.hibernate.criterion.Restrictions;
 import datamodel.Event;
 
 
-public class UtilEvent extends UtilDB{
-	
-	//get all events
+public class UtilEvent extends UtilDB {
+
+	// get all events
 	public static List<Event> listEvents() {
 		List<Event> resultList = new ArrayList<Event>();
 
@@ -39,7 +39,32 @@ public class UtilEvent extends UtilDB{
 		return resultList;
 	}
 
-	//get event with specified eventId
+	// get all events created by a specific user
+	public static List<Event> listEvents(String username) {
+		List<Event> resultList = new ArrayList<Event>();
+
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null;
+
+		try {
+			tx = session.beginTransaction();
+			List<?> events = session.createCriteria(Event.class).add(Restrictions.eq("username", username)).list();
+			for (Iterator<?> iterator = events.iterator(); iterator.hasNext();) {
+				Event event = (Event) iterator.next();
+				resultList.add(event);
+			}
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return resultList;
+	}
+
+	// get event with specified eventId
 	public static Event getEvent(int eventId) {
 		Event event = null;
 
@@ -48,9 +73,9 @@ public class UtilEvent extends UtilDB{
 
 		try {
 			tx = session.beginTransaction();
-			
+
 			event = (Event) session.createCriteria(Event.class).add(Restrictions.eq("eventId", eventId)).uniqueResult();
-			
+
 			tx.commit();
 		} catch (HibernateException e) {
 			if (tx != null)
@@ -62,7 +87,7 @@ public class UtilEvent extends UtilDB{
 		return event;
 	}
 
-	//create a new event
+	// create a new event
 	public static boolean createEvent(String username, String name, String description) {
 		Session session = getSessionFactory().openSession();
 		Transaction tx = null;
